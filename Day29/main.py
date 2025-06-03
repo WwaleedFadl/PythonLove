efbd4ac4b80b163a3e8c0ecf8f8382d505df1042
waleed fadl <wwwaleedfadl@gmail.com>
2025-06-03 18:44:53 +0300

    playing with tkinter

--- Day29/main.py
+++ Day29/main.py
@@ -1,30 +1,37 @@
 from tkinter import *
-            
 # ---------------------------- UI SETUP ------------------------------- #
-# ------------------------------------- Window
 window = Tk()
 window.title("Password Generator")
-window.config(padx=100,pady=50,bg=YELLOW)
-# -------------------------------------  Label Timer
-timer_label = Label(text="Mypass", font=("Arial", 50),fg=GREEN,bg=YELLOW)
-timer_label.grid(column=1,row=0)
+window.config(padx=100,pady=50)
 # ------------------------------------- Canvas
-canvas = Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
-tomato_img = PhotoImage(file='tomato.png')
-canvas.create_image(100,112,image=tomato_img)
-timer_text = canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,'bold'))
-canvas.grid(column=1,row=1)
-#-------------------------------------  Count Down
-#count_down(5)
-# -------------------------------------  Button Marker True  Button 
-# Start Button 
-button_start = Button(text='Start',highlightthickness=0,command=start_timer)
-button_start.grid(column=0,row=2)
-# End Button 
-button_end = Button(text='Reset',highlightthickness=0,command=reset_timer)
-button_end.grid(column=2,row=2)
-#Done Label
-Done_label = Label(font=("Arial", 50),fg=GREEN,bg=YELLOW)
-Done_label.grid(column=1,row=3)
+canvas = Canvas(width=200,height=200,highlightthickness=0)
+logo_img = PhotoImage(file='logo.png')
+canvas.create_image(100,100,image=logo_img)
+canvas.grid(column=1,row=0)
+
+# -------------------------------------  Label Website
+website_label = Label(text="Website", font=("Arial", 10))
+website_label.grid(column=0,row=1)
+website_entry = Entry(width=35)
+website_entry.grid(row=1,column=1,columnspan=2)
+
+################
+email_username_label = Label(text="Email / Username", font=("Arial", 10))
+email_username_label.grid(column=0,row=2)
+email_entry = Entry(width=35)
+email_entry.grid(row=2,column=1,columnspan=2)
+
+################
+password_label = Label(text="Password", font=("Arial", 10))
+password_label.grid(column=0,row=3)
+password_entry = Entry(width=21)
+password_entry.grid(row=3,column=1)
+generate_password_button = Button(text='Generate Password',highlightthickness=0)
+generate_password_button.grid(column=3,row=3)
+
+#######################################################
+add_button = Button(text='Add',highlightthickness=0)
+add_button.grid(column=1,row=4,columnspan=2)
+
 #--------------------------------------------- 
 window.mainloop()
